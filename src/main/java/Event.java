67ae27de4eaab42014491f9334b6056d9b9ae9de
Cookiemunchrr <songyaozhu@gmail.com>
2026-08-18 15:52:52 +0800

    added event class that extends task

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+public class Event extends Task{
+    protected String eventStart;
+    protected String eventEnd;
 
-public class Event {
+    public Event(String task_detail, String eventStart, String eventEnd) {
+        super(task_detail);
+        this.eventStart = eventStart;
+        this.eventEnd = eventEnd;
+    }
+
+    @Override
+    public String toString(){
+        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", eventStart, eventEnd);
+    }
 }
