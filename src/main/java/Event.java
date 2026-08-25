12d28eee45255b442e9f346aeff2b14d01267d42
Cookiemunchrr <songyaozhu@gmail.com>
2026-08-25 19:56:18 +0800

    change datetimes to follow LocalDate

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 public class Event extends Task{
-    protected String eventStart;
-    protected String eventEnd;
+    protected LocalDate eventStart;
+    protected LocalDate eventEnd;
 
     public Event(String task_detail, String eventStart, String eventEnd) {
         super(task_detail);
-        this.eventStart = eventStart;
-        this.eventEnd = eventEnd;
+        this.eventStart = LocalDate.parse(eventStart);
+        this.eventEnd = LocalDate.parse(eventEnd);
     }
 
     @Override
     public String toString(){
-        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", eventStart, eventEnd);
+        return "[E]" + super.toString() + String.format(" (from: %s to: %s)",
+                eventStart.format(DateTimeFormatter.ofPattern("MMM d yyyy")),
+                eventEnd.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 
     @Override
     public String toFileString() {
-        return "E " + super.toFileString() + String.format(" /from %s /to %s", eventStart, eventEnd);
+        return "E " + super.toFileString() + String.format(" /from %s /to %s",
+                eventStart.format(DateTimeFormatter.ofPattern("MMM d yyyy")),
+                eventEnd.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
